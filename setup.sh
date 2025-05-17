49add36962ad77eb399ebbb67f50112069b1fd99
catupeloco <catupeloco@gmail.com>
2025-05-17 01:14:01 -0300

    20250517-0114

--- setup.sh
+++ setup.sh
@@ -30,7 +30,7 @@ while [ "$REPEAT" == "yes" ] ; do
 		    whiptail --title "Local admin creation" --msgbox "ERROR: Passwords dont match, try again" 20 60  3>&1 1>&2 2>&3
 	fi
 done
-
+clear
 cd /tmp
 
 echo "Inicializing logs tails -------------------------------------"
