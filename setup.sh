19b8aca9142e43e17ce7158fa340086dce4dffd7
catupeloco <catupeloco@gmail.com>
2025-08-31 10:48:06 -0300

    20250831-1048

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DATE=20250831-1043
+SCRIPT_DATE=20250831-1048
 echo ahora $(date) script  $SCRIPT_DATE
 sleep 8
 reset # Re-Set terminal for multiple runs
@@ -285,10 +285,10 @@ if [ "$REPARTED" == "yes" ] ; then
 fi
 
 echo "Formating partitions ----------------------------------------"
-[ "$REPARTED" == yes ] && mkfs.vfat -n EFI        ${DEVICE}1    > /dev/null 2>&1
-[ "$REPARTED" == yes ] && mkfs.ext4 -L RESOURCES  ${DEVICE}4    > /dev/null 2>&1
-		 	  mkfs.ext4 -L CLONEZILLA ${DEVICE}2    > /dev/null 2>&1
-			  mkfs.ext4 -L LINUX      ${DEVICE}3    > /dev/null 2>&1
+[ "$REPARTED" == yes ] && mkfs.vfat -I -n EFI        ${DEVICE}1  > /dev/null 2>&1
+[ "$REPARTED" == yes ] && mkfs.ext4 -I -L RESOURCES  ${DEVICE}4  > /dev/null 2>&1
+		 	  mkfs.ext4 -I -L CLONEZILLA ${DEVICE}2  > /dev/null 2>&1
+			  mkfs.ext4 -I -L LINUX      ${DEVICE}3  > /dev/null 2>&1
 
 echo "Mounting ----------------------------------------------------"
 echo "---OS partition"
