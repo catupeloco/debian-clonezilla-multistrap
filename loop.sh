263de0bd2a23afe157c127dd626c0a7bfa6dc77f
catupeloco <catupeloco@gmail.com>
2025-09-06 15:15:01 -0300

    20250906-1515

---
+++ loop.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+while true; do
+	vi setup.sh 
+	sed -i '/^SCRIPT_DATE=/c\SCRIPT_DATE='$(date +'%Y%m%d-%H%M') setup.sh 
+       	git add . 
+	git commit -m "$(date +'%Y%m%d-%H%M')" 
+	git push
+done
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DATE=20250906-1513
+SCRIPT_DATE=20250906-1515
 echo ---------------------------------------------------------------------------
 echo "ahora   "$(env TZ=America/Argentina/Buenos_Aires date +'%Y%m%d-%H%M') 
 echo "script  "$SCRIPT_DATE
