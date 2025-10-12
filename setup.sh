06deec676560e40c42be89f82f9cc162f8ccb903
catupeloco <catupeloco@gmail.com>
2025-10-12 17:15:18 -0300

    20251012-1715

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DATE=20251012-1714
+SCRIPT_DATE=20251012-1715
 echo ---------------------------------------------------------------------------
 echo "ahora   "$(env TZ=America/Argentina/Buenos_Aires date +'%Y%m%d-%H%M') 
 echo "script  "$SCRIPT_DATE
