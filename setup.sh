dd61f71e0ee9725df7b1c3689a34f6ed8290ccdd
catupeloco <catupeloco@gmail.com>
2025-11-07 23:34:17 -0300

    20251107-2334

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DATE=20251107-2326
+SCRIPT_DATE=20251107-2334
 set -e # Exit on error
 LOG=/tmp/laptop.log
 ERR=/tmp/laptop.err
@@ -272,8 +272,13 @@ echo "For remote access during installation, you can connect via ssh
 ########PROGRESS BAR#####################################################
 echo "============================================================="
 set +e
-let "PROGRESS_BAR_PERCENT    = PROGRESS_BAR_CURRENT * 100                / PROGRESS_BAR_MAX"
-let "PROGRESS_BAR_FILLED_LEN = PROGRESS_BAR_CURRENT * PROGRESS_BAR_WIDTH / PROGRESS_BAR_MAX"
+if [ $PROGRESS_BAR_CURRENT -eq $PROGRESS_BAR_MAX ] ; then
+	let "PROGRESS_BAR_PERCENT    = 100"
+	let "PROGRESS_BAR_FILLED_LEN = PROGRESS_BAR_WIDTH"
+else
+	let "PROGRESS_BAR_PERCENT    = PROGRESS_BAR_CURRENT * 100                / PROGRESS_BAR_MAX"
+	let "PROGRESS_BAR_FILLED_LEN = PROGRESS_BAR_CURRENT * PROGRESS_BAR_WIDTH / PROGRESS_BAR_MAX"
+fi
 let "PROGRESS_BAR_EMPTY_LEN  = PROGRESS_BAR_WIDTH   - PROGRESS_BAR_FILLED_LEN"
 PROGRESS_BAR_FILLED_BAR=$(printf '%.s#' $(seq 1 $PROGRESS_BAR_FILLED_LEN))
  PROGRESS_BAR_EMPTY_BAR=$(printf '%.s-' $(seq 1 $PROGRESS_BAR_EMPTY_LEN ))
