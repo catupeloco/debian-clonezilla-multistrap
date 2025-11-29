44d0ea3901887f8da1dc4de23e680cbfc4d85731
catupeloco <catupeloco@gmail.com>
2025-11-29 12:37:45 -0300

    20251129-1237

--- loop.sh
+++ loop.sh
@@ -2,11 +2,20 @@
 #This script is for making changes more quickly
 while true; do
 	vi setup.sh 
+	echo ------------------------------------------------
+	echo Should we abort changes? -----------------------
+	echo ------------------------------------------------
+	sleep 5
+	echo ------------------------------------------------
+	echo Making changes, please wait a couples of seconds
+	echo ------------------------------------------------
 	sed -i '/^SCRIPT_DATE=/c\SCRIPT_DATE='$(date +'%Y%m%d-%H%M') setup.sh 
        	git add . 
 	git commit -m "$(date +'%Y%m%d-%H%M')" 
 	git push
-	echo Sleeping 10 seconds ...
 	grep ^SCRIPT_DATE setup.sh
+	echo ------------------------------------------------
+	echo Changes are done, Sleeping 10 seconds ----------
+	echo ------------------------------------------------
 	sleep 10
 done
--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DATE=20251129-1232
+SCRIPT_DATE=20251129-1237
 set -e # Exit on error
 LOG=/tmp/laptop.log
 ERR=/tmp/laptop.err
