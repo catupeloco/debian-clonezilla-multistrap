bfbbe9fd927ab9d180cd7007b4384641dcd0e8ec
catupeloco <catupeloco@gmail.com>
2025-11-30 22:09:20 -0300

    20251130-2209

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DATE=20251130-2206
+SCRIPT_DATE=20251130-2209
 set -e # Exit on error
 LOG=/tmp/laptop.log
 ERR=/tmp/laptop.err
@@ -339,8 +339,8 @@ set +e
 		setsid bash -c 'exec tail -f '$ERR' 				<> /dev/tty3 >&0 2>&1' &
 		setsid bash -c 'exec watch sudo ls -larth /tmp/resources-fs/*/	<> /dev/tty4 >&0 2>&1' &
 		setsid bash -c 'exec watch sudo fdisk -l			<> /dev/tty5 >&0 2>&1' &
-		setsid bash -c 'exec watch sudo blkid | sort			<> /dev/tty6 >&0 2>&1' &
-		setsid bash -c 'exec watch sudo lsblk | sort			<> /dev/tty7 >&0 2>&1' &
+		setsid bash -c 'exec watch sudo blkid				<> /dev/tty6 >&0 2>&1' &
+		setsid bash -c 'exec watch sudo lsblk				<> /dev/tty7 >&0 2>&1' &
 	fi
 set -e
 
