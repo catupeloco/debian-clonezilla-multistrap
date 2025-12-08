237097a0ed708d0e1163e216290c238d2c4a0a11
catupeloco <catupeloco@gmail.com>
2025-12-08 10:36:54 -0300

    20251208-1036

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-SCRIPT_DATE=20251208-1035
+SCRIPT_DATE=20251208-1036
 set -e # Exit on error
 LOG=/tmp/laptop.log
 ERR=/tmp/laptop.err
@@ -472,7 +472,7 @@ echo "Formating partitions ----------------------------------------"
 			  fsck -y "${DEVICE}"3				 >/dev/null 2>&1 || true
 			  fsck -y "${DEVICE}"4				 >/dev/null 2>&1 || true
 [ "$REPARTED" == yes ] && mkfs.vfat  -n EFI        "${DEVICE}"1 -f	 >/dev/null 2>&1 || mkfs.vfat  -n EFI        "${DEVICE}"1 >/dev/null 2>&1 || true
-[ "$REPARTED" == yes ] && mkfs.ext4  -L RESOURCES  "${DEVICE}"4	-f	 >/dev/null 2>&1 || mkfs.ext4  -L RESOURCES  "${DEVICE}"4 >/dev/null 2>&1 || true
+[ "$REPARTED" == yes ] && mkfs.ext4  -L RESOURCES  "${DEVICE}"4	-f	 >/dev/null 2>&1 # || mkfs.ext4  -L RESOURCES  "${DEVICE}"4 >/dev/null 2>&1 || true
 		 	  mkfs.ext4  -L CLONEZILLA "${DEVICE}"2 -f	 >/dev/null 2>&1 || mkfs.ext4  -L CLONEZILLA "${DEVICE}"2 >/dev/null 2>&1 || true
 			  mkfs.btrfs -L LINUX      "${DEVICE}"3 -f	# >/dev/null 2>&1 || mkfs.btrfs -L LINUX      "${DEVICE}"3 >/dev/null 2>&1 || true
 
