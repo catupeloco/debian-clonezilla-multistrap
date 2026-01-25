e7ce04d121e1c53ed5b8dfcf746b54d32d38ffef
Javier I Galvez <81536776+catupeloco@users.noreply.github.com>
2026-01-25 01:36:12 -0300

    Docker and excalidraw

--- setup.sh
+++ setup.sh
@@ -1483,6 +1483,39 @@ exit
 
 
 # FOR VERSION 2.0
+ # Official Docker
+  # https://docs.docker.com/engine/install/debian/#install-using-the-repository 
+  # Repository 
+  # Auto update 
+cat << EOF
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+EOF
+
+ # Excalidraw docker
+  # Autostart
+  # Update from script 
+  # /etc/hosts alias
+  # Start shortcut
+  # docker build -t excalidraw/excalidraw .
+  # docker run --rm -dit --name excalidraw -p 5000:80 excalidraw/excalidraw:latest
+
 	# Marktext and drawio with gh
 		# Check version before download
 		# Download this way on setup.sh
